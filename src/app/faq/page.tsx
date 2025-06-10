a4d6f88200d8ecd54775bbf2bb69e23467842e0a
Peter Damiano <peterdamiano12masterpro@gmail.com>
2025-06-10 08:16:01 +0000

    Fix these errors listed below ReferenceError: CardContent is not defined

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,10 +1,11 @@
+
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Logo } from "@/components/icons/Logo";
