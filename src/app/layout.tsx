a30de1ca356c619dae1588030fa5b31f4ed48227
Peter Damiano <peterdamianotech@gmail.com>
2025-10-30 07:52:32 +0000

    My app at first it had fixed UI that not zooming on the screen it was fi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <meta name="theme-color" content="#9C27B0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
